6de32ed33f1e41f401720cbc542251126c12c610
Hari Kishore <kishore111pec@pec.edu>
2020-04-14 12:59:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.get('/',(req, res)=>{
-    res.send('hello');
+    res.send('go to <a href="/api/getAllPosts"> /api/getAllPosts</a>');
 });
 
 app.get('/insert', (req, res)=>{
