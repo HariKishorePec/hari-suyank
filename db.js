59dbf3727c2c483129da7a003bca7fb7e7acb99f
Hari Kishore <kishore111pec@pec.edu>
2020-04-14 19:10:01 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = `mongodb+srv://${{secrets.MONGO_DB_USER}}:${{secrets.MONGO_DB_PASSWORD}}@cluster0-pswsd.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${{MONGO_DB_USER}}:${{MONGO_DB_PASSWORD}}@cluster0-pswsd.mongodb.net/test?retryWrites=true&w=majority`;
 const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(uri, {
